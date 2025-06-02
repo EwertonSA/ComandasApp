9798b88adba24748e4f7a9254c28fcaf49b43b1e
EwertonSA <ewerton.60@hotmail.com>
2025-06-02 18:50:26 -0300

    Teste

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -31,19 +31,7 @@ export const adminJs = new AdminJS({
      theme: {
     colors: {
       background:"#151515",
-      primary100: '#ff0043',
-      primary80: '#ff1a57',
-      primary60: '#ff3369',
-      primary40: '#ff4d7c',
-      primary20: '#ff668f',
-      grey100: '#151515',
-      grey80: '#333333',
-      grey60: '#4d4d4d',
-      grey40: '#666666',
-      grey20: '#dddddd',
-      filterBg: '#333333',
-      accent: '#151515',
-      hoverBg: '#151515',
+  
        text: '#ffffff',
        
     }
