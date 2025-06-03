8b6384692206c65afe0c870124057690ff6e1f03
EwertonSA <ewerton.60@hotmail.com>
2025-06-03 09:45:59 -0300

    Teste

--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,14 @@ import { sequelize } from  "./database/index.js"
 import {adminJs, adminJsRouter} from './adminjs/index.js'
 import router from "./routes.js"
 import cors from 'cors'
+import path from 'path';
 const app= express()
 app.use(cors())
 app.use(express.static('public'))
+app.use(
+  '/admin/frontend/assets',
+  express.static(path.join(process.cwd(), 'public/admin/frontend/assets'))
+)
 app.use(adminJs.options.rootPath,adminJsRouter)
 app.use(express.json())
 app.use(router)
