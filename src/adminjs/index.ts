c100b58456cdf9d64e48210b1c11a51ffb36e16c
EwertonSA <ewerton.60@hotmail.com>
2025-06-03 16:06:46 -0300

    teste2

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -28,14 +28,22 @@ export const adminJs = new AdminJS({
 
   branding: {
     companyName: 'Seu Painel Admin',
-     theme: {
+  theme: {
     colors: {
-      background: '#ffffff',
-      text: '#151515',
-      border: '#cccccc',
-      tableHeader: '#f4f4f4',
-    },
-    
+      primary100: '#ff0043',
+      primary80: '#ff1a57',
+      primary60: '#ff3369',
+      primary40: '#ff4d7c',
+      primary20: '#ff668f',
+      grey100: '#151515',
+      grey80: '#333333',
+      grey60: '#4d4d4d',
+      grey40: '#666666',
+      grey20: '#dddddd',
+      filterBg: '#333333',
+      accent: '#151515',
+      hoverBg: '#151515',
+    }
   }
   }
 })
