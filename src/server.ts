e57f496d7dad70fba09418c2c1ae9c7b8d6d2b07
EwertonSA <ewerton.60@hotmail.com>
2025-06-04 19:11:39 -0300

    mudando a porta!

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ app.use(express.static('public'))
 app.use(adminJs.options.rootPath,adminJsRouter)
 app.use(express.json())
 app.use(router)
-const PORT= process.env.PORT || 3000
+const PORT= process.env.PORT || 3001
 
 app.listen(PORT, ()=>{
     sequelize.authenticate().then(()=>{
