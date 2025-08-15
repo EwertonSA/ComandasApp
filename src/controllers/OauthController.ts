9801582b23c21b9de69f084dff3b44615b5ea780
EwertonSA <ewerton.60@hotmail.com>
2025-08-15 15:25:33 -0300

    Ajustando redirect da função de callback do Facebook

--- src/controllers/OauthController.ts
+++ src/controllers/OauthController.ts
@@ -29,7 +29,7 @@ export const OauthController={
 
 
   
-res.redirect('http://localhost:3000/employeeApp')
+res.redirect('http://esadev.com.br/employeeApp')
   } catch (error) {
   console.error('Erro na autenticação:', error);
   return res.status(500).json({ message: 'Erro ao autenticar com Facebook' });
