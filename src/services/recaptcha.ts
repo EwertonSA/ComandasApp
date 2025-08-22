6ce9ccacf98a995292aa168e4da663f902b4268d
EwertonSA <ewerton.60@hotmail.com>
2025-08-22 13:29:04 -0300

    Adicionando reCAPTCHA ao login

--- src/services/recaptcha.ts
+++ src/services/recaptcha.ts
@@ -5,7 +5,7 @@ import axios from "axios";
 const validateRecaptcha = async (
   token: string
 )=> {
-  const secret = process.env.RECAPTCHA_SECRET_KEY as string;
+  const secret = process.env.RECAPTCHA_SECRET as string;
 
   const res = await axios.post(
     "https://www.google.com/recaptcha/api/siteverify",
